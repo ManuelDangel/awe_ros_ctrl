84cdec7a141f088dbc68751238022416c89c9318
Manuel Dangel <mdangel@ethz.ch>
2018-03-13 22:26:08 +0100

    configured many parts for Eclipse IDE, google style, not building anymore

--- include/fw_nmpc.h
+++ include/fw_nmpc.h
@@ -21,13 +21,13 @@
 #include "acado_auxiliary_functions.h"
 
 /* some convenient definitions */
-#define NX	ACADO_NX	/* Number of differential state variables.  */
-#define NU 	ACADO_NU	/* Number of control inputs. */
-#define NOD	ACADO_NOD	/* Number of online data values. */
-#define NY 	ACADO_NY 	/* Number of measurements/references on nodes 0..N - 1. */
-#define NYN	ACADO_NYN	/* Number of measurements/references on node N. */
-#define N	ACADO_N		/* Number of intervals in the horizon. */
-#define NX_AUGM 2		/* Number of augmented differential state variables. */
+#define NX  ACADO_NX   /* Number of differential state variables.  */
+#define NU  ACADO_NU   /* Number of control inputs. */
+#define NOD ACADO_NOD  /* Number of online data values. */
+#define NY  ACADO_NY   /* Number of measurements/references on nodes 0..N - 1.*/
+#define NYN ACADO_NYN  /* Number of measurements/references on node N. */
+#define N ACADO_N      /* Number of intervals in the horizon. */
+#define NX_AUGM 2      /* Number of augmented differential state variables. */
 
 /* global variables used by the solver. */
 ACADOvariables acadoVariables;
@@ -40,112 +40,104 @@ namespace fw_nmpc {
  * class that implements fixed-wing nmpc
  */
 class FwNMPC {
+ public:
+  FwNMPC();
 
-public:
+  /* callbacks */
+  void globPosCb(const sensor_msgs::NavSatFix::ConstPtr& msg);
+  void odomCb(const nav_msgs::Odometry::ConstPtr& msg);
+  void waypointListCb(const mavros_msgs::WaypointList::ConstPtr& msg);
+  void currentWpCb(const std_msgs::Int32::ConstPtr& msg);
+  void homeWpCb(const mavros_msgs::HomePosition::ConstPtr& msg);
 
-	FwNMPC();
+  /* initializations */
+  int initNMPC();
+  void initACADOVars();
+  void initHorizon();
 
-	/* callbacks */
-	void 	aslctrlDataCb(const mavros_msgs::AslCtrlData::ConstPtr& msg);
-	void 	globPosCb(const sensor_msgs::NavSatFix::ConstPtr& msg);
-	void 	odomCb(const nav_msgs::Odometry::ConstPtr& msg);
-	void 	ekfExtCb(const mavros_msgs::AslEkfExt::ConstPtr& msg);
-	void 	nmpcParamsCb(const mavros_msgs::AslNmpcParams::ConstPtr& msg);
-	void 	waypointListCb(const mavros_msgs::WaypointList::ConstPtr& msg);
-	void 	currentWpCb(const std_msgs::Int32::ConstPtr& msg);
-	void 	homeWpCb(const mavros_msgs::HomePosition::ConstPtr& msg);
-	void 	aslctrlDebugCb(const mavros_msgs::AslCtrlDebug::ConstPtr& msg);
+  /* sets */
+  void updateACADO_X0();
+  void updateACADO_OD();
+  void updateACADO_Y();
+  void updateACADO_W();
 
-	/* initializations */
-	int 	initNMPC();
-	void 	initACADOVars();
-	void 	initHorizon();
+  /* gets */
+  double getLoopRate();
+  double getTimeStep();
+  void reqSubs();
+  void calculateTrackError(const real_t *in);
 
-	/* sets */
-	void 	updateACADO_X0();
-	void	updateACADO_OD();
-	void	updateACADO_Y();
-	void	updateACADO_W();
+  /* functions */
+  void update();
+  int nmpcIteration();
 
-	/* gets */
-	double	getLoopRate();
-	double 	getTimeStep();
-	void 	reqSubs();
-	void 	calculateTrackError(const real_t *in);
+  /* publishing encapsulation */
+  void publishControls(uint64_t &t_ctrl, uint64_t t_iter_approx,
+                       int obctrl_status);
+  void publishAcadoVars();
+  void publishNmpcInfo(ros::Time t_iter_start, uint64_t t_ctrl,
+                       uint64_t t_solve, uint64_t t_update, uint64_t t_wp_man);
 
-	/* functions */
-	void 	update();
-	int 	nmpcIteration();
+  double LOOP_RATE;
+  double TSTEP;
+  int FAKE_SIGNALS;
 
-	/* publishing encapsulation */
-	void	publishControls(uint64_t &t_ctrl, uint64_t t_iter_approx, int obctrl_status);
-	void	publishAcadoVars();
-	void	publishNmpcInfo(ros::Time t_iter_start, uint64_t t_ctrl, uint64_t t_solve, uint64_t t_update, uint64_t t_wp_man);
+ private:
+  /* subscription data */
+  Subscriptions subs_;
 
-	double	LOOP_RATE;
-	double 	TSTEP;
-	int 	FAKE_SIGNALS;
+  /* node handles */
+  ros::NodeHandle nmpc_;
 
-private:
+  /* subscribers */
+  ros::Subscriber glob_pos_sub_;
+  ros::Subscriber odom_sub_;
+  ros::Subscriber nmpc_params_sub_;
+  ros::Subscriber waypoint_list_sub_;
+  ros::Subscriber current_wp_sub_;
+  ros::Subscriber home_wp_sub_;
 
-	/* subscription data */
-	Subscriptions 	subs_;
+  /* publishers */
+  ros::Publisher obctrl_pub_;
+  ros::Publisher nmpc_info_pub_;
+  ros::Publisher acado_vars_pub_;
 
-	/* node handles */
-	ros::NodeHandle nmpc_;
+  /* time keeping */
+  ros::Time t_lastctrl;
 
-	/* subscribers */
-	ros::Subscriber aslctrl_data_sub_;
-	ros::Subscriber glob_pos_sub_;
-	ros::Subscriber odom_sub_;
-	ros::Subscriber ekf_ext_sub_;
-	ros::Subscriber nmpc_params_sub_;
-	ros::Subscriber waypoint_list_sub_;
-	ros::Subscriber current_wp_sub_;
-	ros::Subscriber home_wp_sub_;
-	ros::Subscriber aslctrl_debug_sub_;
+  /* controller switch */
+  bool bModeChanged;
+  int last_ctrl_mode;
+  int obctrl_en_;
 
-	/* publishers */
-	ros::Publisher obctrl_pub_;
-	ros::Publisher nmpc_info_pub_;
-	ros::Publisher acado_vars_pub_;
+  /* path definitions */
+  int last_wp_idx_;
+  PathManager paths_;
 
-	/* time keeping */
-	ros::Time t_lastctrl;
+  /* continuity */
+  bool bYawReceived;
+  float last_yaw_msg_;
 
-	/* controller switch */
-	bool bModeChanged;
-	int	last_ctrl_mode;
-	int obctrl_en_;
+  /* track error */
+  float track_error_lat_;
+  float track_error_lon_;
+  double T_b_lat_;
 
-	/* path definitions */
-	int last_wp_idx_;
-	PathManager paths_;
+  /* weight scalers */
+  float W_scale_[NY];
 
-	/* continuity */
-	bool bYawReceived;
-	float last_yaw_msg_;
+  /* control offsets / saturations / normalizations */  // TODO: this should probably not be hard-coded.
+  // uT, phi_ref, theta_ref
+  const double CTRL_DEADZONE[3] = { 0.2, 0.0, 0.0 };  // zero-based deadzone TODO: non-zero-based? asymmetric?
+  // const double CTRL_OFFSET[3] = {0.0, 0.0, 0.0}; // constant offset TODO: how to use?
+  const double CTRL_NORMALIZATION[3] = { 0.8, 1.0, 1.0 };  // normlization !!must not be zero!!
+  const double CTRL_SATURATION[3][2] = { { 0.0, 1.0 }, { -0.5236, 0.5236 }, {
+      -0.2618, 0.2618 } };  // these are saturations for the internal model ATM* TODO: probably should incorporate some saturation for the incoming controls as well
 
-	/* track error */
-	float track_error_lat_;
-	float track_error_lon_;
-	double T_b_lat_;
-
-	/* weight scalers */
-	float W_scale_[NY];
-
-	/* control offsets / saturations / normalizations */ //TODO: this should probably not be hard-coded.
-	// uT, phi_ref, theta_ref
-	const double CTRL_DEADZONE[3] = {0.2, 0.0, 0.0}; // zero-based deadzone TODO: non-zero-based? asymmetric?
-//	const double CTRL_OFFSET[3] = {0.0, 0.0, 0.0}; // constant offset TODO: how to use?
-	const double CTRL_NORMALIZATION[3] = {0.8, 1.0, 1.0}; // normlization !!must not be zero!!
-	const double CTRL_SATURATION[3][2] = { {0.0, 1.0}, {-0.5236, 0.5236}, {-0.2618, 0.2618} }; // these are saturations for the internal model ATM* TODO: probably should incorporate some saturation for the incoming controls as well
-
-	/* node functions */
-	void 	shutdown();
-
+  /* node functions */
+  void shutdown();
 };
 
-} // namespace fw_nmpc
+}  // namespace fw_nmpc
 
-#endif
+#endif  // INCLUDE_FW_NMPC_H_
--- include/subs.h
+++ include/subs.h
@@ -6,16 +6,11 @@
 #include <ros/console.h>
 
 // INCLUDES for mavros
-#include <mavros_msgs/AslCtrlData.h>
-#include <mavros_msgs/AslEkfExt.h>
-#include <mavros_msgs/AslNmpcParams.h>
 #include <sensor_msgs/NavSatFix.h>
 #include <nav_msgs/Odometry.h>
 #include <mavros_msgs/WaypointList.h>
 #include <std_msgs/Int32.h>
-#include <geographic_msgs/GeoPoint.h>
 #include <geometry_msgs/Vector3Stamped.h>
-#include <mavros_msgs/AslCtrlDebug.h>
 #include <mavros_msgs/HomePosition.h>
 
 /*
@@ -27,14 +22,10 @@ class Subscriptions
 
 public:
 
-	mavros_msgs::AslCtrlData 		aslctrl_data;
 	sensor_msgs::NavSatFix	 		glob_pos;
 	nav_msgs::Odometry 				odom;
-	mavros_msgs::AslEkfExt			ekf_ext;
-	mavros_msgs::AslNmpcParams		nmpc_params;
 	mavros_msgs::WaypointList		waypoint_list;
 	std_msgs::Int32					current_wp;
-	geographic_msgs::GeoPoint		home_wp;
 
 };
 
